2b0d5dc0bcd2338c90ea56847ad61eeb8f670445
“isavov” <iliasavov@gmail.com>
2022-11-20 11:43:49 +0200

    update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,8 +19,9 @@ resource "google_compute_network" "vpc" {
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  name                     = "${var.project_id}-subnet"
+  region                   = var.region
+  network                  = google_compute_network.vpc.name
+  ip_cidr_range            = "10.10.0.0/24"
+  private_ip_google_access = true
 }
